26b188338fbfa7831ef95a2324bcaa95c5001865
Bobby Spencer <bobby@twire.se>
2017-05-12 09:16:33 +0200

    Updated landing page text and menu Scaffolded Customers and Pruchases with both Sync and Async

---
+++ ClientVsServer/Controllers/PurchasesAsyncController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClientVsServer.Models;
+
+namespace ClientVsServer.Controllers
+{
+    public class PurchasesAsyncController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: PurchasesAsync
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Purchases.ToListAsync());
+        }
+
+        // GET: PurchasesAsync/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = await db.Purchases.FindAsync(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // GET: PurchasesAsync/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PurchasesAsync/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,FirstName,LastName,CardNo")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Purchases.Add(purchase);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(purchase);
+        }
+
+        // GET: PurchasesAsync/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = await db.Purchases.FindAsync(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // POST: PurchasesAsync/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,FirstName,LastName,CardNo")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(purchase).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(purchase);
+        }
+
+        // GET: PurchasesAsync/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = await db.Purchases.FindAsync(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // POST: PurchasesAsync/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Purchase purchase = await db.Purchases.FindAsync(id);
+            db.Purchases.Remove(purchase);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

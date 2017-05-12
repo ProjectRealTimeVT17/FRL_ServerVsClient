c6a2bd938625fd2a01e9caad99164f979a5fdba1
Bobby Spencer <bobby@twire.se>
2017-05-12 09:03:18 +0200

    Moved DbContext Added Customer and Purchase Enabled migrations and added first migration

---
+++ ClientVsServer/Models/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ClientVsServer.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ ClientVsServer/Models/Purchase.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientVsServer.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CardNo { get; set; }
+    }
+}

d64e9dca70a2227f56e074a40eb3398cbaeb3961
Bobby Spencer <bobby@twire.se>
2017-05-12 08:44:00 +0200

    Add project files.

---
+++ ClientVsServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClientVsServer.Startup))]
+namespace ClientVsServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
